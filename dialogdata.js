88943cf86f79db9895ca759eb166338eca9850ec
Shih Oon Liong <shihoon@mechaloid.com>
2013-07-13 22:47:15 -0700

    wip

--- dialogdata.js
+++ dialogdata.js
@@ -1,20 +1,50 @@
 var dialogData =  {
-    meetdog : [
+
+    // Initial Spawn
+    start : [
+        {
+            character: 'penny',
+            emotion: null,
+            dialog: 'Well, I left the park, and then I walked down the street, but then, instead of turning at 35th, I turned at 34th, because they sound the same and the house on the corner is the same color and I forgot.',
+            sound: null,
+            sound_description: '',
+            delay: 2000
+        },
+        {
+            character: 'game',
+            emotion: null,
+            dialog: 'Use the arrow keys to move left and right. Hit Space to jump.',
+            sound: null,
+            sound_description: null,
+            delay: 2000
+        },
+    ],
+
+    // First view of dog
+    meetdog: [
+        {
+            character: 'penny',
+            emotion: null,
+            dialog: 'And then... then there were these big dogs! (Barks loudly) They were really loud and mean and nasty, and I was scared.',
+            sound: null,
+            sound_description: null,
+            delay: 2000
+        },
         {
             character: 'penny',
             emotion: null,
             dialog: 'Luckily I had my sparkly taser of awesome!',
             sound: null,
             sound_description: 'Record scratch',
-            delay: 1000
+            delay: 2000
         },
         {
-            character: 'mother',
+            character: 'mum',
             emotion: null,
             dialog: 'What? A taser? I never gave you a taser! You could put your eye out with one of those...',
             sound: null,
             sound_description: null,
-            delay: 1000
+            delay: 2000
         },
         {
             character: 'penny',
@@ -22,7 +52,7 @@ var dialogData =  {
             dialog: 'Sparkly taser of awesome! Mine! It\'s my super special weapon and I use it all the time to beat the bad guys.',
             sound: null,
             sound_description: 'insistent',
-            delay: 1000
+            delay: 2000
         },
         {
             character: 'penny',
@@ -30,7 +60,7 @@ var dialogData =  {
             dialog: 'Now, can I finish my story?',
             sound: null,
             sound_description: 'petulant',
-            delay: 1000
+            delay: 2000
         },
         {
             character: 'game',
@@ -38,7 +68,43 @@ var dialogData =  {
             dialog: 'Press the \'T\' Key to hold out your taser',
             sound: null,
             sound_description: null,
-            delay: 1000
+            delay: 4000
         },
-    ]
+    ],
+
+    killedfirstminion: [
+        {
+            character: 'penny',
+            emotion: null,
+            dialog: 'I hit it with my taser, and then! and then! It went zap and boom, and *ZZZZT*, and then it DIED!',
+            sound: null,
+            sound_description: null,
+            delay: 2000
+        },
+        {
+            character: 'mum',
+            emotion: null,
+            dialog: '...so now your taser is killing small animals.',
+            sound: null,
+            sound_description: null,
+            delay: 2000
+        },
+        {
+            character: 'penny',
+            emotion: null,
+            dialog: 'Yuh-huh! And it went all lit up, and I could see its skeleton like in the cartoons, and it danced around a little bit, then it went POOF in a little pile of soot.',
+            sound: null,
+            sound_description: null,
+            delay: 2000
+        },
+        {
+            character: 'mum',
+            emotion: null,
+            dialog: 'All right, that's it. No more Warner Brother cartoons before bedtime',
+            sound: null,
+            sound_description: null,
+            delay: 2000
+        },
+    ],
+
 };

70799acaf2b6e1f256157889c21f7b276bd87019
Shih Oon Liong <shihoon@mechaloid.com>
2013-07-13 21:53:11 -0700

    More letter

---
+++ dialogdata.js
@@ -0,0 +1,44 @@
+var dialogData =  {
+    meetdog : [
+        {
+            character: 'penny',
+            emotion: null,
+            dialog: 'Luckily I had my sparkly taser of awesome!',
+            sound: null,
+            sound_description: 'Record scratch',
+            delay: 1000
+        },
+        {
+            character: 'mother',
+            emotion: null,
+            dialog: 'What? A taser? I never gave you a taser! You could put your eye out with one of those...',
+            sound: null,
+            sound_description: null,
+            delay: 1000
+        },
+        {
+            character: 'penny',
+            emotion: null,
+            dialog: 'Sparkly taser of awesome! Mine! It\'s my super special weapon and I use it all the time to beat the bad guys.',
+            sound: null,
+            sound_description: 'insistent',
+            delay: 1000
+        },
+        {
+            character: 'penny',
+            emotion: null,
+            dialog: 'Now, can I finish my story?',
+            sound: null,
+            sound_description: 'petulant',
+            delay: 1000
+        },
+        {
+            character: 'game',
+            emotion: null,
+            dialog: 'Press the \'T\' Key to hold out your taser',
+            sound: null,
+            sound_description: null,
+            delay: 1000
+        },
+    ]
+};
